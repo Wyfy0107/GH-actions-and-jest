458ad8ab37071118facec910603ad0339e93a0b3
Duy Nguyen <nguyenduy010798@gmail.com>
2020-09-07 00:11:20 +0300

    initial commit

---
+++ src/Account.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Account({ user }) {
+  return (
+    <div>
+      <p>david@gmail.com</p>
+    </div>
+  );
+}
+
+export default Account;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import App from "./App";
+import Account from "./Account";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders App header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Display Active Users Account Details</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+it("renders Account component", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.contains(<Account user='duy' />)).toEqual(true);
 });
